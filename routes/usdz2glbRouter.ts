decdae2c37ca6331c05ed47c7626ef66b13a4394
tastekim <tastekim@kakao.com>
2023-02-01 11:15:36 +0900

    spawn 실행 옵션 python3 로 수정

--- routes/usdz2glbRouter.ts
+++ routes/usdz2glbRouter.ts
@@ -10,11 +10,11 @@ const usdz2glb = router.post('/usdz2glb', async (ctx: any, next: Next) => {
         const filePath = ctx.request.files.file.filepath; // 업로드하는 파일의 경로
         const usdzFileName = ctx.request.files.file.originalFilename; // 업로드하는 usdz 파일 이름
 
-        // 확장자가 포함된 파일 이름 문자열에서 확장자 바꾸기 .glb => .usdz
+        // 확장자가 포함된 파일 이름 문자열에서 확장자 바꾸기 .usdz => .glb
         const convertName = usdzFileName.split('.');
-        convertName[1] = 'usdz';
+        convertName[1] = 'glb';
         const glbFileName = convertName.join('.');
-        const usdz2glbConvertProcess = spawn('python', ['usdz2glb.py', filePath, glbFileName]);
+        const usdz2glbConvertProcess = spawn('python3', ['usdz2glb.py', filePath, glbFileName]);
         usdz2glbConvertProcess.stdout.on('data', (data: Buffer) => {
             console.log(data.toString());
             ctx.status = 200;
