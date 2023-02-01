e9669e858d2e90b9260126ce871970c0e001e53e
tastekim <tastekim@kakao.com>
2023-02-01 17:07:00 +0900

    uploadFiles.sh curl 성공 시 exit 0 으로 수정.

--- uploadFiles.sh
+++ uploadFiles.sh
@@ -2,7 +2,7 @@
 STATUS=$(curl -w %{http_code} http://$1/upload/usdzUpload -F file1=@./tmp/$2 -F file2=@$3)
 INTSTATUS=$(($STATUS+0))
 if [ $INTSTATUS -eq 204 ]; then
-  sh ./setproductobj.sh $2 $3
+  exit 0
 else
   exit 64
 fi
