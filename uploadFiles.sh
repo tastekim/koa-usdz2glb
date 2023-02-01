7277722267d5e8cbc066f5c15cecb34a8845cc9e
tastekim <tastekim@kakao.com>
2023-02-01 17:10:33 +0900

    uploadFiles.sh 경로 수정

--- uploadFiles.sh
+++ uploadFiles.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-STATUS=$(curl -w %{http_code} http://$1/upload/usdzUpload -F file1=@./tmp/$2 -F file2=@$3)
+STATUS=$(curl -w %{http_code} http://$1/fileUpload/upload2GCS -F file1=@./tmp/$2 -F file2=@$3)
 INTSTATUS=$(($STATUS+0))
 if [ $INTSTATUS -eq 204 ]; then
   exit 0
