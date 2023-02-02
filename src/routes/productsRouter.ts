24f00efa8ac131ea9f5ecdea8010d48ccee54f84
tastekim <tastekim@kakao.com>
2023-02-02 19:26:00 +0900

    firestore CRUD 추가

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -1,28 +1,78 @@
 import { Context, Next } from 'koa';
 import koaBody from 'koa-body';
 import Router from '@koa/router';
-import { getAllData } from '../modules/firestore-func';
+import { getAllData, getData, deleteData } from '../modules/firestore-func';
 
 const router = new Router({
     prefix : '/products',
 });
 
-const productsRouter = router.get('/', async (ctx: any, next: Next) => {
-    try {
-        const dataList = await getAllData();
-        ctx.status = 200;
-        ctx.response.body = { dataList };
-        console.log(`request /products getAllData Success.`)
-        await next();
-    } catch (err) {
-        if (err instanceof Error) {
-            ctx.status ??= 500;
-            ctx.response.body = {
-                message : err.message
-            };
-            console.error(err.stack);
+const productsRouter = router
+    .get('/', async (ctx: any, next: Next) => {
+        try {
+            const dataList = await getAllData();
+            ctx.status = 200;
+            ctx.response.body = { dataList };
+            console.log(`request /products getAllData Success.`);
+            await next();
+        } catch (err) {
+            if (err instanceof Error) {
+                ctx.status ??= 500;
+                ctx.response.body = {
+                    message : err.message
+                };
+                console.error(err.stack);
+            }
         }
-    }
-});
+    })
+    .get('/:productId', async (ctx: any, next: Next) => {
+        try {
+            const { productId } = ctx.params;
+            const productData = await getData(productId);
+            if (productData instanceof Error) {
+                ctx.status = 400;
+                ctx.throw(productData.message);
+            } else {
+                ctx.response.body = productData;
+                console.log(`${productId} found.`);
+            }
+            await next();
+        } catch
+            (err: unknown) {
+            if (err instanceof Error) {
+                console.log(err.message);
+                console.log(err.stack);
+                ctx.status ??= 500;
+                ctx.response.body = {
+                    message : err.message
+                };
+            }
+        }
+    })
+    .delete('/:productId', async (ctx: any, next: Next) => {
+        try {
+            const { productId } = ctx.params;
+            const result = await deleteData(productId);
+            if (result instanceof Error) {
+                ctx.status = 400;
+                ctx.throw(result.message);
+            }
+            ctx.status = 204;
+            ctx.response.body = {
+                success : true
+            };
+            console.log(`${productId} => delete success`);
+            await next();
+        } catch (err) {
+            if (err instanceof Error) {
+                console.log(err.message);
+                console.log(err.stack);
+                ctx.status ??= 500;
+                ctx.response.body = {
+                    message : err.message
+                };
+            }
+        }
+    });
 
 export { productsRouter };
--- uploadFiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-STATUS=$(curl -w %{http_code} http://$1/fileUpload/upload2GCS -F file1=@./tmp/$2 -F file2=@$3)
-INTSTATUS=$(($STATUS+0))
-if [ $INTSTATUS -eq 204 ]; then
-  exit 0
-else
-  exit 64
-fi
