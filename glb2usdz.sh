737da8ff33e0fc40d242ac6b50780bc5375622a9
tastekim <tastekim@kakao.com>
2023-02-06 12:19:56 +0900

    오탈자 수정

--- glb2usdz.sh
+++ glb2usdz.sh
@@ -4,7 +4,7 @@ BASEPATH=$(dirname "$0")
 export PATH=$PATH:$BASEPATH/USD:$PATH:$BASEPATH/usdzconvert;
 export PYTHONPATH=$PYTHONPATH:$BASEPATH/USD/lib/python
 usdzconvert $2 ../tmp/$1
-if [ -e ../temp/$1 ]; then
+if [ -e ../tmp/$1 ]; then
   exit 0
 else
   exit 64
