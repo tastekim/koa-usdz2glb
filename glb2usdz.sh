99bfcf6330ca24c9b83a2b68000cab91d5b80978
tastekim <tastekim@kakao.com>
2023-02-06 12:22:43 +0900

    shell script if 문 삭제

--- glb2usdz.sh
+++ glb2usdz.sh
@@ -4,8 +4,3 @@ BASEPATH=$(dirname "$0")
 export PATH=$PATH:$BASEPATH/USD:$PATH:$BASEPATH/usdzconvert;
 export PYTHONPATH=$PYTHONPATH:$BASEPATH/USD/lib/python
 usdzconvert $2 ../tmp/$1
-if [ -e ../tmp/$1 ]; then
-  exit 0
-else
-  exit 64
-fi
